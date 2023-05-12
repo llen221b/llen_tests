6d285cff7a482a2f7939d98b786027501a524490
llen221b <lena.gakman@gmail.com>
2023-05-12 15:49:02 +0200

    if password is longer than 9, skip cond2

--- password.py
+++ password.py
@@ -1,6 +1,8 @@
 def is_acceptable_password(password: str) -> bool:
     cond1 = len(password) >= 6
     cond2 = any(map(str.isdigit, password))
+    if len(password) >= 9:
+        return True
     if password == "1234567":
         return False
     return all([cond1, cond2])
