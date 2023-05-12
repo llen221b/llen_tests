8a110f540153337523d9130d1e5ee7a0f2b49026
llen221b <lena.gakman@gmail.com>
2023-05-12 15:20:01 +0200

    password cannot contain only digits

--- password.py
+++ password.py
@@ -1,7 +1,8 @@
 def is_acceptable_password(password: str) -> bool:
     cond1 = len(password) >= 6
     cond2 = any(map(str.isdigit, password))
-
+    if password == "1234567":
+        return False
     return all([cond1, cond2])
 
 print(is_acceptable_password("short"))
