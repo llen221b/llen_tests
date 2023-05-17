9fe92d160411d9041983c15085d785751ec03283
llen221b <lena.gakman@gmail.com>
2023-05-17 14:44:02 +0200

    Password cannot contain word "password"

--- password.py
+++ password.py
@@ -1,11 +1,16 @@
 def is_acceptable_password(password: str) -> bool:
-    cond1 = len(password) >= 6
-    cond2 = any(map(str.isdigit, password))
-    if len(password) >= 9:
-        return True
-    if password == "1234567":
-        return False
-    return all([cond1, cond2])
+    cond1 = len(password) > 6
+    cond2 = any(char.isdigit() for char in password)
+    cond3 = any(char.isalpha() for char in password)
+    if len(password) > 9:  # cond4
+        cond2 = cond3 = True
 
+    cond4 = 'password' not in password.lower()
+    cond5 = 'PASSWORD' not in password.upper()
+
+    return all([cond1, cond2, cond3, cond4])
+
+
+print("Example:")
 print(is_acceptable_password("short"))
 
